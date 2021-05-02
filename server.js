df67de54fcc94546c9013e7b7a0b9646bb1ce79b
Brianna Cerkiewicz <briannac@shaw.ca>
2021-05-02 21:35:10 +0000

    remove widget example material

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+
 
 //B's
 const itemsRoutes = require('./routes/items');
@@ -38,7 +38,6 @@ const itemsRoutes = require('./routes/items');
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
 
 // B's
 app.use("/users", itemsRoutes(db));
