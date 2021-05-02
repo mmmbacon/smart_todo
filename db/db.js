7293c9ac918b3a11f52ed2ae950a5c7df496e0f2
Brandon <bmacdonald1986@gmail.com>
2021-05-02 15:39:58 +0000

    Add dotenv to db file so it can actually read the environment variables

--- db/db.js
+++ db/db.js
@@ -2,6 +2,12 @@
 const { Pool } = require('pg');
 const dbParams = require('../lib/db.js');
 const db = new Pool(dbParams);
-db.connect();
+db.connect()
+  .then((msg) => {
+    console.log(msg);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 module.exports = db;
