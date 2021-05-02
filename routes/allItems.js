9c8d6099217306ac8daba076de9dd88a11327129
Brianna Cerkiewicz <briannac@shaw.ca>
2021-05-02 15:52:23 +0000

    update endpoint with allItems function (to query db)

--- routes/allItems.js
+++ routes/allItems.js
@@ -1,18 +1,73 @@
 const express = require('express');
+const allItems = require('../db/allItems.js');
 const router = express.Router();
+const { sortCategories } = require('./helpers.js');
 
 module.exports = {
-  //likely not really needed since we're never showing the full, unsorted list of items
   getItems: (db) => {
     //route is just "/" because full route is already in server.js
     router.get("/", (req, res) => {
-
+      allItems(1)
+        .then(res => {
+          res.json({ res }) //this was in the user examples, let's see what it does
+          return res.rows
+        })
+        .catch(error => {
+          console.log(error);
+          res
+          .status(500)
+          .json({ error: err.message })
+        });
     });
     return router;
   },
   //Create a new item
   postItems: (db) => {
     router.get("/", (req, res) => {
+
+      //check for keywords (db? just an array?)
+
+      //if no match, count amount of rows in books, movies, restaurants database (Brandon to supply query to replace null)
+
+      const apiPriority = {
+        books: null,
+        movies: null,
+        products: null
+      }
+
+      //get the category with the highest count
+      const sortedCategories = sortCategories(apiPriority);
+
+      // query the apis in order
+
+      for (const category of sortedCategories) {
+        if (category === 'books') {
+          //call function that returns book api data
+          if ('it was in the api' === true) {
+            return 'it was in the api, its a book' //add to database
+          }
+          continue; //i think i can cut these actually
+
+        }
+        if (category === 'movies') {
+          //call function that returns movie api data
+          if ('it was in the api' === true) {
+            return 'it was in the api, its a movie' //add to database
+          }
+
+          continue;
+        }
+        if (category === 'eateries') {
+          //call function that returns movie api data
+          if ('it was in the api' === true) {
+            return 'it was in the api, its an eatery' //add to database
+          }
+          continue;
+        }
+      }
+
+      //if it was none of those things, add to database as a product
+
 
     });
     return router;
