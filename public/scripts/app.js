84cc08a5cff471995c5925a1b4276a5a585f25f0
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-02 23:44:17 +0000

    Pull items dynamically from database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,57 +10,6 @@
 // });
 
 $(document).ready(function() {
-  const itemData =  [
-    {
-      id: 1,
-      user_id: 1,
-      category_id: 1,
-      description: "Watch Superstore on Netflix",
-      date_created: "2021-05-02T20:45:03.434Z",
-      date_due: null,
-      priority: null,
-      completed: false,
-      email: "sarah.worthington@hotmail.com",
-      password: "password"
-    },
-    {
-      id: 1,
-      user_id: 1,
-      category_id: 3,
-      description: "Drink a cappucino",
-      date_created: "2021-05-02T20:45:03.434Z",
-      date_due: null,
-      priority: null,
-      completed: false,
-      email: "sarah.worthington@hotmail.com",
-      password: "password"
-    },
-    {
-      id: 1,
-      user_id: 1,
-      category_id: 4,
-      description: "Buy a new sofa",
-      date_created: "2021-05-02T20:45:03.434Z",
-      date_due: null,
-      priority: null,
-      completed: false,
-      email: "sarah.worthington@hotmail.com",
-      password: "password"
-    },
-    {
-      id: 1,
-      user_id: 1,
-      category_id: 2,
-      description: "Read The Hobbit",
-      date_created: "2021-05-02T20:45:03.434Z",
-      date_due: null,
-      priority: null,
-      completed: false,
-      email: "sarah.worthington@hotmail.com",
-      password: "password"
-    }
-  ];
-
   // Create HTML to display each item
   const createItem = function(item) {
     const displayItem = `
@@ -71,24 +20,34 @@ $(document).ready(function() {
 
   const renderItems = function(items) {
     for (const item of items) {
+      const $item = createItem(item.description)
+
       if (item.category_id === 1) {
-        const $item = createItem(item.description);
         $('#watch-container').append($item);
       }
       if (item.category_id === 2) {
-        const $item = createItem(item.description);
         $('#read-container').append($item);
       }
       if (item.category_id === 3) {
-        const $item = createItem(item.description);
         $('#eat-container').append($item);
       }
       if (item.category_id === 4) {
-        const $item = createItem(item.description);
         $('#buy-container').append($item);
       }
     }
   }
 
-  renderItems(itemData);
+  // Display existing items on page load
+  const loadItems = function() {
+    $.ajax('/users/1/items', {
+      method: 'GET',
+      dataType: 'JSON'
+    }).then((items) => {
+      renderItems((items.items));
+    }).catch((err) => {
+      console.log('Error: ', err);
+    });
+  };
+
+  loadItems();
 });
