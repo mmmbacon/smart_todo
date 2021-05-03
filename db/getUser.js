c97a87f2d765b0e12955d5812663bfa4234885cd
Brandon <bmacdonald1986@gmail.com>
2021-05-03 18:05:12 +0000

    Add getUser query which returns user data

---
+++ db/getUser.js
@@ -0,0 +1,29 @@
+const db = require('./db');
+
+/**
+ * Postgres query function for returning a user with given password
+ * @param { string } email The user email address
+ * @returns { object } the user object
+ */
+const getUser = function(email) {
+
+  const queryString = `
+  SELECT users.email, users.first_name, users.last_name, COUNT(*) AS total_item_count
+  FROM items
+  JOIN users ON items.user_id = users.id
+  WHERE users.email = $1
+  GROUP BY users.email, users.first_name, users.last_name
+  `;
+  const params = [`${email}`];
+
+  return db.query(queryString, params)
+    .then((res)=> {
+      console.log(res.rows[0]);
+      return res.rows[0];
+    })
+    .catch((err)=> {
+      return err;
+    });
+};
+
+module.exports = getUser;
