f24970f4d4131b264928837364b0dcb305cd7c40
Brianna Cerkiewicz <briannac@shaw.ca>
2021-05-03 16:31:34 +0000

    fill in book function and work on eatery function

--- routes/helpers.js
+++ routes/helpers.js
@@ -48,11 +48,32 @@ const isItAMovie = function (userEntry) {
 //     return;
 //   })
 
-
+//Book logic
 const isItABook = function (userEntry) {
+  return new Promise((res, rej) => {
+    request(`https://www.googleapis.com/books/v1/volumes?q=${userEntry}&key=${process.env.BOOK_KEY}`, function (error, response, body) {
+      if (error) rej(error);
+      const bookTitle = JSON.parse(body).items[0].volumeInfo.title;
+      console.log('title:', bookTitle)
 
-
+      if (bookTitle.length > 0) {
+        res(true);
+      } else {
+        res(false);
+      }
+    });
+  });
 };
+
+//testcode
+isItABook('bedlamstacks')
+  .then(res => {
+    // console.log('res:',res)
+    if (res) {
+      console.log('yes, it is a book, add to database as a book')
+    }
+    return;
+  })
 
 //Eatery logic
 const getLocation = function () {
@@ -61,7 +82,7 @@ const getLocation = function () {
       if (error) rej(error);
       const location = [];
       location.push(JSON.parse(body).city, JSON.parse(body).region);
-      console.log('location',location);
+      console.log('location', location);
       res(location);
     });
   });
@@ -73,49 +94,55 @@ const isItAnEatery = function (userEntry) {
 
   //how to call this correctly?
   const location = getLocation()
-    .then(response => {
-      console.log(response)
+    .then(res => {
+      console.log('location array is', res)
+      return res;
     })
-    .catch((data, status) => {
-      console.log('Request failed');
+    .then(res => {
+      //from yelp api documentation starts here
       const apiKey = process.env.EATERY_KEY;
-
 
       const searchRequest = {
         term: userEntry,
-        location: `${location[0]}, ${location[1]}`
+        location: `${res[0]}, ${res[1]}`
       };
-
 
       const client = yelp.client(apiKey);
 
-      return client.search(searchRequest)
-      .then(response => {
-        const firstResult = response.jsonBody.businesses[0];
-        const name = firstResult.name;
-        console.log('firstresult',firstResult)
-        if (name.length > 0) {
-          console.log('name', name);
+      client.search(searchRequest)
+        .then(response => {
+          const firstResult = response.jsonBody.businesses[0];
+          const name = firstResult.name;
+          // console.log('firstresult',firstResult) //to see the restaurant's real location
+          if (name.length > 0) {
+            console.log('name', name);
             return name;
           }
           return;
         }).catch(error => {
           console.log(error);
         });
+
+
     })
+
+    .catch((data, status) => {
+      console.log('Request failed');
+    })
+
 
 
 };
 
 //test code
-isItAnEatery('the keg')
-  .then(res => {
-    console.log('res:', res)
-    if (res) {
-      console.log('yes, it is an eatery, add to database as an eatery')
-    }
-    return;
-  })
+// isItAnEatery('lordoftherings')
+// .then(res => {
+//   console.log('res:', res)
+//   if (res) {
+//     console.log('yes, it is an eatery, add to database as an eatery')
+//   }
+//   return;
+// })
 
 
 module.exports = { sortCategories, isItAMovie, isItABook, isItAnEatery }
