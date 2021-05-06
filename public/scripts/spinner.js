1a0d3362b38cb5037c2a0eb4d063afac9bd3fffc
Brandon <bmacdonald1986@gmail.com>
2021-05-06 21:55:14 +0000

    Updated ejs to add spinner class

--- public/scripts/spinner.js
+++ public/scripts/spinner.js
@@ -11,6 +11,7 @@ $(document)
     $loading.find('.progress-bar').addClass('visible progress-bar-rgb progress-bar-striped progress-bar-animated');
     $loading.find('.progress-bar').removeClass('invisible');
     $loading.find('.progress-bar').text('Waving the magic wand');
+    $hippo.addClass('svg-rgb');
   })
   .ajaxStop(function() {
     $loading.find('.progress-bar').removeClass('visible progress-bar-rgb progress-bar-striped progress-bar-animated');
